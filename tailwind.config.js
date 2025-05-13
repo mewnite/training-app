2ae43087b699d8ea3e35aee0fecd0745355e993d
mewnite <pabluchin16@gmail.com>
2025-05-12 22:11:54 -0300

    a

---
+++ tailwind.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"], // Incluye todos los componentes y archivos JS/JSX
+  theme: {
+    extend: {}, // Personalizaciones opcionales
+  },
+  plugins: [], // Añade plugins si es necesario
+};
